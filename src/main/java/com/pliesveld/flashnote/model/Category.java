0c8d291df931b24001ed1dfc3dec63c013851d7c
Patrick Liesveld <pliesveld@users.noreply.github.com>
2015-12-01 17:23:24 -0500

    default key type set by database provider for Category

--- src/main/java/com/pliesveld/flashnote/model/Category.java
+++ src/main/java/com/pliesveld/flashnote/model/Category.java
@@ -8,6 +8,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -19,7 +20,7 @@ import javax.persistence.Table;
 public class Category implements Serializable
 {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "CATEGORY_ID")
     private Short id;
 
