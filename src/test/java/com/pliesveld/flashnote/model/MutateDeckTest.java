6089ef8648f172ea2b88dc11dcf2f2917317c5c6
Patrick Liesveld <pliesveld@users.noreply.github.com>
2016-01-13 14:51:17 -0500

    Mutated Deck unit tests Deck maps a list of FlashCards Unit tests stores a deck, and then modifies the list order

---
+++ src/test/java/com/pliesveld/flashnote/model/MutateDeckTest.java
@@ -0,0 +1,217 @@
+package com.pliesveld.flashnote.model;
+
+import com.pliesveld.config.SpringTestConfig;
+import org.hibernate.*;
+import org.hibernate.criterion.Projections;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringTestConfig.class, loader = AnnotationConfigContextLoader.class)
+public class MutateDeckTest
+{
+    @Autowired
+    SessionFactory sessionFactory;
+
+    @Test
+    public void sessionFactoryWired()
+    {
+        assertNotNull(sessionFactory);
+    }
+
+    @Test
+    public void sessionHasCurrent()
+    {
+        Session session = sessionFactory.openSession();
+        assertNotNull(session);
+
+        assertTrue(session.isOpen());
+        assertTrue(session.isConnected());
+        session.close();
+    }
+
+    @Test
+    public void verifyEmpty()
+    {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction().begin();
+        {
+            Query query = session.createQuery("SELECT count(*) FROM com.pliesveld.flashnote.model.Deck");
+            Long count = (Long)query.uniqueResult();
+            assertTrue("Deck is zero",count == 0);
+        }
+
+        {
+            Criteria crit = session.createCriteria(Deck.class);
+            crit.setProjection(Projections.rowCount());
+            Long count_long = (Long)crit.uniqueResult();
+            int count = count_long.intValue();
+            assertEquals("Database rows should be empty",0,count);
+        }
+
+        Class<?>[] classArray = {Student.class,Question.class,Answer.class,FlashCard.class,Deck.class,Category.class};
+
+        for(Class<?> tableClass : classArray)
+        {
+            Criteria crit = session.createCriteria(tableClass);
+            crit.setProjection(Projections.rowCount());
+            Long count_long = (Long)crit.uniqueResult();
+            int count = count_long.intValue();
+            assertEquals("Database rows should be empty",0,count);
+
+        }
+
+        session.getTransaction().commit();
+    }
+
+
+    private void clearDeck()
+    {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction().begin();
+        SQLQuery sql;
+
+        sql = session.createSQLQuery("DELETE FROM DECK_FLASHCARD;");
+        sql.executeUpdate();
+
+        sql = session.createSQLQuery("DELETE FROM DECK;");
+        sql.executeUpdate();
+
+        sql = session.createSQLQuery("DELETE FROM FLASHCARD;");
+        sql.executeUpdate();
+
+        sql = session.createSQLQuery("DELETE FROM QUESTION;");
+        sql.executeUpdate();
+        sql = session.createSQLQuery("DELETE FROM ANSWER;");
+        sql.executeUpdate();
+
+        session.getTransaction().commit();
+    }
+
+    @Test
+    public void createDeck()
+    {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        int i = 1;
+        int a_no = 0;
+        int q_no = 0;
+
+        {
+            Criteria crit = session.createCriteria(FlashCard.class);
+            crit.setProjection(Projections.rowCount());
+            assertEquals("Database should have 0 flashcards",0,((Long)crit.uniqueResult()).intValue());
+        }
+
+
+        Deck deck = new Deck();
+        deck.setTitle("This is an example Deck.");
+
+        List<FlashCard> list = new LinkedList<>();
+
+        Question que = new Question();
+        que.setContent("This is a question?");
+        session.save(que);
+
+        do {
+            Answer ans = new Answer();
+            ans.setContent(String.format("%d", a_no++));
+            session.save(ans);
+
+            FlashCard fc = new FlashCard(que,ans);
+
+            session.save(fc);
+            list.add(fc);
+
+        } while(i++ < 5);
+
+        deck.setFlashCards(list);
+        session.save(deck);
+
+        {
+            Criteria crit = session.createCriteria(FlashCard.class);
+            crit.setProjection(Projections.rowCount());
+            assertEquals("Database should have 5 flashcards",5,((Long)crit.uniqueResult()).intValue());
+        }
+
+        {
+            Criteria crit = session.createCriteria(Deck.class);
+            crit.setProjection(Projections.rowCount());
+            assertEquals("Database should have 1 deck",1,((Long)crit.uniqueResult()).intValue());
+        }
+
+        Serializable deck_id = deck.getId();
+
+        {
+            Deck deck2 = session.load(Deck.class,deck_id);
+            assertEquals("Decks should be equal",deck,deck2);
+
+            assertNotNull("Loading a deck had a null list",deck2.getFlashCards());
+            assertFalse("Loaded deck should have elements",deck2.getFlashCards().isEmpty());
+            assertEquals("Loaded deck should have 5 elements", 5, deck2.getFlashCards().size());
+
+
+            List<FlashCard> deck_list = deck2.getFlashCards();
+
+            String expected_string[] = {"0","1","2","3","4"};
+
+            int j = 0;
+            for(FlashCard fc : deck_list)
+            {
+                assertEquals("Fetched answer differed", expected_string[j], fc.getAnswer().getContent());
+                j++;
+            }
+
+            FlashCard fc_move = deck_list.remove(1);
+            deck_list.add(fc_move);
+            session.saveOrUpdate(deck2);
+
+        }
+
+        deck = null;
+
+        {
+            Deck deck3 = session.load(Deck.class,deck_id);
+
+            assertNotNull("Loading a deck had a null list",deck3.getFlashCards());
+            assertFalse("Loaded deck should have elements",deck3.getFlashCards().isEmpty());
+            assertEquals("Loaded deck should have 5 elements",5,deck3.getFlashCards().size());
+
+
+            List<FlashCard> deck_list = deck3.getFlashCards();
+
+            String expected_string[] = {"0","2","3","4","1"};
+
+            int j = 0;
+            for(FlashCard fc : deck_list)
+            {
+                assertEquals("Fetched answer differed", expected_string[j], fc.getAnswer().getContent());
+                j++;
+            }
+
+        }
+
+
+
+        session.getTransaction().rollback();
+    }
+
+
+
+}
+
