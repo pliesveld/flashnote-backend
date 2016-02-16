00daf6968ad326fbb592e322e66766c09c7ea72c
Patrick Liesveld <pliesveld@users.noreply.github.com>
2016-02-15 23:14:42 -0500

    Student deck modification unit tests

--- src/test/java/com/pliesveld/flashnote/domain/StudentDeckTest.java
+++ src/test/java/com/pliesveld/flashnote/domain/StudentDeckTest.java
@@ -12,6 +12,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.junit.Assert.*;
+
 import java.io.Serializable;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -23,15 +25,82 @@ public class StudentDeckTest
     private EntityManager entityManager;
 
     @Test
-    @Transactional
-    public void studentPersistGenId()
+    public void studentDeckConstruct()
     {
         Student student1 = new Student();
         student1.setName("Student1");
         student1.setEmail("student1@email.com");
+        student1.setPassword("password");
+                
+        Question que = new Question("Question?");
+        Answer ans = new Answer("Answer.");
+        
+        entityManager.persist(que);
+        entityManager.persist(ans);
+        
+        FlashCard fc = new FlashCard(que,ans);
+        Deck deck = new Deck();
+        deck.getFlashCards().add(fc);
+        student1.getDecks().add(deck);
+        
         entityManager.persist(student1);
         entityManager.flush();
-        Serializable s1 = student1.getId();
+
+    }
+    
+    @Test
+    public void studentDeckRemove()
+    {
+        Serializable sid = null; 
+        Serializable did = null;
+        
+        
+    
+        {
+            Student student1 = new Student();
+            student1.setName("Student1");
+            student1.setEmail("student1@email.com");
+            student1.setPassword("password");
+                    
+            Question que = new Question("Question?");
+            Answer ans = new Answer("Answer.");
+            
+            entityManager.persist(que);
+            entityManager.persist(ans);
+            
+            FlashCard fc = new FlashCard(que,ans);
+            Deck deck = new Deck();
+            deck.getFlashCards().add(fc);
+            student1.getDecks().add(deck);
+            
+            entityManager.persist(student1);
+            entityManager.flush();
+            
+            did = deck.getId();
+            sid = student1.getId();
+        }
+        
+        assertNotNull(sid);
+        Student student = entityManager.find(Student.class, sid);
+        assertNotNull(student);
+        
+        Deck deck = entityManager.find(Deck.class,did);
+        
+        student.getDecks().remove(deck);
+        
+        FlashCard fc = deck.getFlashCards().get(0);
+        
+        Question que = fc.getQuestion();
+        Answer ans = fc.getAnswer();
+        
+        entityManager.remove(que);
+        entityManager.remove(ans);
+        
+        entityManager.remove(fc);
+        
+        entityManager.remove(deck);
+        entityManager.merge(student);
+        entityManager.flush();
     }
 
 }
