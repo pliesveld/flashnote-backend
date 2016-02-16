b27928a722c4a9a64f3078cdb37e7b2539507f70
Patrick Liesveld <pliesveld@users.noreply.github.com>
2016-02-15 22:47:18 -0500

    refactored category test to new test case

--- src/test/java/com/pliesveld/flashnote/domain/SampleCardTest.java
+++ src/test/java/com/pliesveld/flashnote/domain/SampleCardTest.java
@@ -52,55 +52,6 @@ public class SampleCardTest
          
     }
 
-    @Test
-    public void generateCategorySingle()
-    {
-        Category category = new Category();
-        category.setName("ROOT");
-        category.setDescription("ROOT category description");
-        entityManager.persist(category);
-        entityManager.flush();
-        Serializable new_id = category.getId();
-
-        Category category_retrieved = entityManager.find(Category.class, new_id);
-        assertNotNull(category_retrieved);
-        assertEquals("Retrieved same as persisted", category.getName(), category_retrieved.getName());
-    }
-
-    @Test
-    public void generateCategoryHierarchy()
-    {
-        Category category = new Category();
-        category.setName("ROOT");
-        category.setDescription("ROOT category description");
-        entityManager.persist(category);
-        entityManager.flush();
-
-
-        Category chld = new Category();
-        chld.setName("CHILD");
-        chld.setDescription("Child category description.");
-        
-        category.addChildCategory(chld);
-        entityManager.persist(chld);
-        
-        /*
-        Serializable new_id = category.getId();
-
-        Category category_retrieved = entityManager.find(Category.class, new_id);
-        assertNotNull(category_retrieved);
-        Set<?> set_child = category_retrieved.getChildCategories();
-        assertNotNull(set_child);
-        assertEquals(set_child.size(), 1);
-
-        Category chld_retrieved = (Category) set_child.iterator().next();
-        assertEquals("Retrieved same as persisted", category.getName(), category_retrieved.getName());
-        assertEquals("Child Retrieved same as persisted", chld.getName(), chld_retrieved.getName());
-        */
-
-         
-    }
-
     @Test 
     public void testQuestion()
     {
