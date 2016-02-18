70d8b7fcc13029655bc3c9fdfa1b9e002f265add
Patrick Liesveld <pliesveld@users.noreply.github.com>
2016-02-17 21:28:33 -0500

    Unless otherwise specified, default profile to local

--- src/main/java/com/pliesveld/flashnote/spring/FlashnoteContainerApplication.java
+++ src/main/java/com/pliesveld/flashnote/spring/FlashnoteContainerApplication.java
@@ -1,12 +1,17 @@
 package com.pliesveld.flashnote.spring;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class FlashnoteContainerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FlashnoteContainerApplication.class, args);
+        System.setProperty("spring.profiles.default", System.getProperty("spring.profiles.default", "local"));
+        SpringApplication.run(FlashnoteContainerApplication.class, args);
 	}
 }
