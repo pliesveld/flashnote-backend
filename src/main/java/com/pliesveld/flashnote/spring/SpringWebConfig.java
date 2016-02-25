43ec510baaaf4730fb4e2914cf726dc1403c6c0b
Patrick Liesveld <pliesveld@users.noreply.github.com>
2016-02-25 10:18:26 -0500

    Added multipart config to spring mvc adapter. Added initialization bean for Spring-validators and related method resolution processor

--- src/main/java/com/pliesveld/flashnote/spring/SpringWebConfig.java
+++ src/main/java/com/pliesveld/flashnote/spring/SpringWebConfig.java
@@ -1,16 +1,16 @@
 package com.pliesveld.flashnote.spring;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.hibernate.validator.HibernateValidator;
+import org.springframework.boot.context.embedded.MultipartConfigFactory;
+import org.springframework.context.annotation.*;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import javax.servlet.MultipartConfigElement;
 import java.text.SimpleDateFormat;
 
 /**
@@ -19,12 +19,34 @@ import java.text.SimpleDateFormat;
 @Configuration
 @EnableWebMvc
 @ComponentScan({
-        "com.pliesveld.flashnote.web.domain",
+        "com.pliesveld.flashnote.web.service",
         "com.pliesveld.flashnote.web.controller",
-        "com.pliesveld.flashnote.web.handler"
+        "com.pliesveld.flashnote.web.handler",
+        "com.pliesveld.flashnote.web.validator"
+
 })
 @PropertySource(value = { "classpath:application.properties" })
 public class SpringWebConfig extends WebMvcConfigurerAdapter{
+
+
+    @Bean
+    @Primary
+    public javax.validation.Validator localValidatorFactoryBean() {
+        LocalValidatorFactoryBean factoryBean = new LocalValidatorFactoryBean();
+        factoryBean.setProviderClass(HibernateValidator.class);
+        return factoryBean;
+    }
+
+    @Bean
+    public MethodValidationPostProcessor methodValidationPostProcessor()
+    {
+        MethodValidationPostProcessor processor
+                = new MethodValidationPostProcessor();
+
+        processor.setValidator(this.localValidatorFactoryBean());
+        return processor;
+    }
+
 
     /**
      * Static resource requests
@@ -39,13 +61,25 @@ public class SpringWebConfig extends WebMvcConfigurerAdapter{
         registry.addResourceHandler("/img/**").addResourceLocations("/img/"); // .setCachePeriod(31556926);
     }
 
-
     @Bean
-    MultipartResolver multipartResolver()
-    {
-        return new StandardServletMultipartResolver();
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory multipartConfigFactory = new MultipartConfigFactory();
+        multipartConfigFactory.setMaxFileSize("25MB");
+        multipartConfigFactory.setMaxRequestSize("50MB");
+        return multipartConfigFactory.createMultipartConfig();
     }
 
+/*
+    @Bean
+    MultipartResolver multipartResolver() throws IOException {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxInMemorySize(1024*1024*5);
+        multipartResolver.setMaxUploadSizePerFile(1024*1024*5*5);
+        multipartResolver.setMaxUploadSize(1024*1024*5*5*5);
+        multipartResolver.setUploadTempDir(new FileSystemResource("/tmp/upload/"));
+        return multipartResolver;
+    }
+*/
     @Bean
     public Jackson2ObjectMapperBuilder jacksonBuilder() {
 	    Jackson2ObjectMapperBuilder b = new Jackson2ObjectMapperBuilder();
