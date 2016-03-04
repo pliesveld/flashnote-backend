cab4b7a75c114aa7d1d80d95040786421467889e
Patrick Liesveld <pliesveld@users.noreply.github.com>
2016-03-04 09:26:28 -0500

    Refactored student service

--- src/main/java/com/pliesveld/flashnote/service/StudentService.java
+++ src/main/java/com/pliesveld/flashnote/service/StudentService.java
@@ -6,24 +6,33 @@ import com.pliesveld.flashnote.domain.StudentDetails;
 import com.pliesveld.flashnote.exception.StudentCreateException;
 import com.pliesveld.flashnote.exception.StudentNotFoundException;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Transactional
+@Validated
 public interface StudentService {
 
-    Student create(String name, String email, String password) throws StudentCreateException;
+    Student create(@NotNull String name,@NotNull String email,@NotNull String password) throws StudentCreateException;
 
-    Student findByEmail(String email)                        throws StudentNotFoundException;
+    @Transactional(readOnly = true)
+    Student findByEmail(@NotNull String email)                    throws StudentNotFoundException;
 
     //StudentDetails             create(StudentDetails studentDetails) throws StudentCreateException;
-    StudentDetails delete(int id)          throws StudentNotFoundException;
+    StudentDetails delete(int id)                        throws StudentNotFoundException;
     StudentDetails update(StudentDetails studentDetails) throws StudentNotFoundException;
 
+    @Transactional(readOnly = true)
     StudentDetails findStudentDetailsById(int id)        throws StudentNotFoundException;
+    @Transactional(readOnly = true)
     Student findStudentById(int id)                      throws StudentNotFoundException;
 
+    @Transactional(readOnly = true)
     Long                count();
+    @Transactional(readOnly = true)
     Iterable<StudentDetails>   findAll();
+    @Transactional(readOnly = true)
     List<Deck>          findDecksByOwner(int id);
 }
--- src/main/java/com/pliesveld/flashnote/service/StudentServiceImpl.java
+++ src/main/java/com/pliesveld/flashnote/service/StudentServiceImpl.java
@@ -4,19 +4,17 @@ import com.pliesveld.flashnote.domain.Deck;
 import com.pliesveld.flashnote.domain.Student;
 import com.pliesveld.flashnote.domain.StudentDetails;
 import com.pliesveld.flashnote.domain.StudentRole;
-import com.pliesveld.flashnote.exception.ResourceNotFoundException;
 import com.pliesveld.flashnote.exception.StudentCreateException;
 import com.pliesveld.flashnote.exception.StudentNotFoundException;
+import com.pliesveld.flashnote.repository.DeckRepository;
 import com.pliesveld.flashnote.repository.StudentDetailsRepository;
 import com.pliesveld.flashnote.repository.StudentRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service(value = "studentService")
@@ -30,8 +28,10 @@ public class StudentServiceImpl implements StudentService {
     @Resource
     StudentDetailsRepository studentDetailsRepository;
 
+    @Resource
+    DeckRepository deckRepository;
+
     @Override
-    @Transactional(rollbackFor = StudentCreateException.class)
     public Student create(String name, String email, String password) throws StudentCreateException {
         
         Student other = studentRepository.findOneByEmail(email);
@@ -61,40 +61,31 @@ public class StudentServiceImpl implements StudentService {
     }
 
     @Override
-    @Transactional(rollbackFor = ResourceNotFoundException.class)
     public List<Deck> findDecksByOwner(int id) {
-        StudentDetails studentDetails = findStudentDetailsById(id);
-        List<Deck> decks = new ArrayList<>();
-        studentDetails.getDecks().forEach(decks::add);
-        return decks;
+        return deckRepository.findByAuthor_Id(id);
     }
 
     @Override
-    @Transactional(readOnly = true)
     public StudentDetails findStudentDetailsById(int id) {
         return studentDetailsRepository.findOne(id);
     }
 
     @Override
-    @Transactional(readOnly = true)
     public Student findStudentById(int id) {
         return studentRepository.findOne(id);
     }
 
     @Override
-    @Transactional(readOnly = true)
     public Iterable<StudentDetails> findAll() {
         return studentDetailsRepository.findAll();
     }
 
     @Override
-    @Transactional(readOnly = true)
     public Student findByEmail(String email) {
         return studentRepository.findOneByEmail(email);
     }
 
     @Override
-    @Transactional(rollbackFor = StudentNotFoundException.class)
     public StudentDetails delete(int id) throws StudentNotFoundException {
         StudentDetails deletedStudentDetails = studentDetailsRepository.findOne(id);
         if(deletedStudentDetails == null)
@@ -105,7 +96,6 @@ public class StudentServiceImpl implements StudentService {
     }
 
     @Override
-    @Transactional(rollbackFor = StudentNotFoundException.class)
     public StudentDetails update(StudentDetails studentDetails) throws StudentNotFoundException {
         StudentDetails updatedStudentDetails = studentDetailsRepository.findOne(studentDetails.getId());
 
@@ -117,7 +107,6 @@ public class StudentServiceImpl implements StudentService {
     }
 
     @Override
-    @Transactional(readOnly = true)
     public Long count() {
         return studentDetailsRepository.count();
     }
