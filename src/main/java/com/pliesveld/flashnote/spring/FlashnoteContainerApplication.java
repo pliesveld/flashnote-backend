c55b3553a66ea3b77d634f0c1c911536a1a4725e
Patrick Liesveld <pliesveld@users.noreply.github.com>
2016-03-03 19:39:07 -0500

    Set default profile for Spring Application to local

--- src/main/java/com/pliesveld/flashnote/spring/FlashnoteContainerApplication.java
+++ src/main/java/com/pliesveld/flashnote/spring/FlashnoteContainerApplication.java
@@ -4,17 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-@PropertySource("classpath:application-local.properties")
 public class FlashnoteContainerApplication {
 
 	public static void main(String[] args) {
-        System.setProperty("spring.profiles.default", System.getProperty("spring.profiles.default", "local"));
-        System.setProperty("my.security.enabled", System.getProperty("my.security.enabled","true"));
+//        System.setProperty("spring.profiles.default", System.getProperty("spring.profiles.default", "local"));
+        System.setProperty("spring.profiles.active",Profiles.LOCAL);
         SpringApplication.run(FlashnoteContainerApplication.class, args);
 	}
 }
